8a83816ddd49ad40d4979c426b274dea371e8dec
blink <natethornton11@gmail.com>
2023-11-29 20:01:47 -0500

    gave up and separated the restarting and git cmd

--- commands/admin/restart.js
+++ commands/admin/restart.js
@@ -1,11 +1,10 @@
-const exec = require('child_process').exec;
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 
 	data: new SlashCommandBuilder()
-		.setName('git')
+		.setName('restart')
 		.setDescription('bot admin command.'),
 	async execute(interaction) {
 		if (interaction.member.id != '160853902726660096') {
@@ -27,24 +26,18 @@ module.exports = {
 			});
 		}
 
-		exec('git pull', function(err, stdout, stderr) {
-			const embed = new EmbedBuilder()
-				.setColor('#ff6633')
-				.setTitle('Git Pull')
-				.setThumbnail('https://i.imgur.com/MsYPWMV.png')
-				.setAuthor({
-					name: 'blink.dclxvi',
-					iconURL: 'https://i.imgur.com/C0ZRGdo.gif',
-					url: 'https://twitch.tv/dotblink',
-				})
-				.setDescription('```xl\n' + stdout + '\n' + stderr + '\n```')
-				.setFooter({
-					text: 'epic',
-				});
+        await interaction.reply({
+            content: 'Restarting.',
+        });
 
-			interaction.reply({
-				embeds: [embed],
-			});
-		});
+        try {
+            process.exit(1);
+        } catch (err) {
+            return interaction.reply({
+                content: 'There was an error restarting the bot. Please go restart the process manually.',
+                fetchReply: true,
+                ephemeral: false,
+            });
+        }
 	},
 };
