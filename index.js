4184fb31d993e7ccb06f2747e98907833729b5fb
blink <natethornton11@gmail.com>
2023-12-01 19:08:24 -0500

    topgg poster support

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const { ShardingManager } = require('discord.js');
+const { AutoPoster } = require('topgg-autoposter');
 
 const manager = new ShardingManager('./bot.js', { token: process.env.discord_token });
+const poster = AutoPoster(process.env.topgg_token, manager);
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-
+manager.on('shardCreate', shard => console.log(`Launched shard #${shard.id}`));
 manager.spawn();
+
+poster.on('posted', (stats) => {
+    console.log('[ [1;34mTOP.GG STATS[0m ] \n' + stats + '\n[ [1;35mEND[0m ]');
+});
+
+poster.on('err', (err) => {
+    console.log('[ [1;31mTOP.GG STATS[0m ] \n' + err + '\n[ [1;35mERROR END[0m ]');
+});
