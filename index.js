0273b2458af6952494f0c1fe17ecc9d6100c7af6
blink <natethornton11@gmail.com>
2023-12-01 19:17:21 -0500

    topgg support changes

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ manager.on('shardCreate', shard => console.log(`Launched shard #${shard.id}`));
 manager.spawn();
 
 poster.on('posted', (stats) => {
-    console.log('[ [1;34mTOP.GG STATS[0m ] \n' + stats + '\n[ [1;35mEND[0m ]');
+    console.log('[ [1;34mTOP.GG STATS[0m ] \n' + stats.toString() + '\n[ [1;35mEND[0m ]');
 });
 
 poster.on('err', (err) => {
-    console.log('[ [1;31mTOP.GG STATS[0m ] \n' + err + '\n[ [1;35mERROR END[0m ]');
+    console.log('[ [1;31mTOP.GG STATS[0m ] \n' + err.toString() + '\n[ [1;35mERROR END[0m ]');
 });
