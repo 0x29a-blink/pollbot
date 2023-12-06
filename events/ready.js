ea41b3133cbb39f9a1d8665b66b653770a0a1579
blink <natethornton11@gmail.com>
2023-12-05 23:28:23 -0500

    logging

--- events/ready.js
+++ events/ready.js
@@ -2,12 +2,20 @@ const {
 	Events,
 } = require('discord.js');
 const chalk = require('chalk'),
-	log = console.log;
+	log = console.log,
+	logtable = console.table;
 
-module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		log(chalk`{green ${client.user.id} Ready.}`);
-	},
-};
+	module.exports = {
+		name: Events.ClientReady,
+		once: true,
+		execute(client) {
+			log(chalk`{green ${client.user.id} Ready.}`);
+			const guilds = client.guilds.cache.filter(guild => guild.memberCount > 1000);
+			const tableData = guilds.map(guild => ({
+				name: guild.name,
+				id: guild.id,
+				memberCount: guild.memberCount,
+			}));
+			logtable(tableData, ['name', 'id', 'memberCount']);
+		},
+	};
