e30f0c730d90ca357b389c8fdbd95886e5693243
blink <natethornton11@gmail.com>
2023-12-20 17:03:17 -0500

    relocated interaction.deferUpdate changed interaction.reply to .followUp

--- src/events/singleVote_pollNewVoter.js
+++ src/events/singleVote_pollNewVoter.js
@@ -15,9 +15,13 @@ module.exports = {
 
 		if (interaction.customId != 'poll') return 0;
 
+		await interaction.deferUpdate().catch(err => {
+			logerr(chalk`{red [ DEFER UPDATE ERROR ]} {gray pollUpdateVote.js}\n${err}\n{red [ END ]}`);
+		});
+
 		const pollExistsInDatabase = await query(`SELECT EXISTS(SELECT messageId FROM polls WHERE messageId=${interaction.message.id})`);
 		if (!pollExistsInDatabase.rows[0].exists === true) {
-			return interaction.reply({
+			return interaction.followUp({
 				content: getLocalization('noLongerExistsInDatabase'),
 				ephemeral: true,
 			}).catch(err => {
@@ -29,9 +33,7 @@ module.exports = {
 		if (userHasVoted.rows[0].exists === true) return 0;
 
 		const userChoice = interaction.values[0];
-		await interaction.deferUpdate().catch(err => {
-			logerr(chalk`{red [ DEFER UPDATE ERROR ]} {gray pollUpdateVote.js}\n${err}\n{red [ END ]}`);
-		});
+
 
 		try {
 			await query('INSERT INTO polls (messageId, pollVoteUserName, pollVoteUserId, pollVoteUserItem) VALUES($1, $2, $3, $4)', [interaction.message.id, interaction.member.displayName, interaction.member.id, userChoice]);
