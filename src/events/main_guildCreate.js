ceefbe4371e061a5ed727d72320b73b94a1f7869
blink <natethornton11@gmail.com>
2024-01-31 03:24:59 -0500

    experimenting with porting spb to cpp
    got bored, kinda want to experiment how well a bot can run on cpp

--- src/events/main_guildCreate.js
+++
@@ -1,9 +0,0 @@
-const { chalk, log } = require('../util/logger');
-
-module.exports = {
-	name: 'guildCreate',
-	once: false,
-	execute(guild) {
-		log(chalk`{magenta [ GUILD ADDED ]} "${guild.name}" [${guild.id}](${guild.memberCount}) at ${Date(Date.now())}`);
-	},
-};
--- src/events/main_guildDelete.js
+++
@@ -1,9 +0,0 @@
-const { chalk, log } = require('../util/logger');
-
-module.exports = {
-	name: 'guildDelete',
-	once: false,
-	execute(guild) {
-		log(chalk`{magenta [ GUILD REMOVED ]} "${guild.name}" [${guild.id}](${guild.memberCount}) at ${Date(Date.now())}`);
-	},
-};
